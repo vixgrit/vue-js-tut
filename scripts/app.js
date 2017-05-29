f6135fee65ea0b8304d37d5287d81279f71048da
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-05-28 21:37:47 -0400

    counter increase ex. and mousemove handler

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,17 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'Hello World!',
-        link: 'http://google.com',
-        finishedLink: '<a href="http://google.com">Google</a>'
+        counter: 0,
+        x: 0,
+        y: 0
     },
     methods: {
-        sayHello: function() {
-            this.title = 'Hello';
-            return this.title;
+        increase: function() {
+            this.counter++;
+        },
+        updateCoordinates: function(event) {
+            this.x = event.clientX;
+            this.y = event.clientY;
         }
     }
 });
