5e0a162fae383d7373d5b6ef3a8f103110d98e64
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-05-29 16:32:48 -0400

    vue dynamic styling practice starter template

--- scripts/app.js
+++ scripts/app.js
@@ -1,27 +1,3 @@
 new Vue({
-    el: '#app',
-    data: {
-        counter: 0,
-        secondCounter: 0
-    },
-    computed: {
-        output: function() {
-            console.log('Computed');
-            return this.counter > 5 ? 'Greater than 5' : 'Less than or equal to 5';
-        }
-    },
-    watch: {
-        counter: function(value) {
-            var vi = this;
-            setTimeout(function() {
-                vi.counter = 0;
-            },2000);
-        }
-    },
-    methods: {
-        result() {
-            console.log('Method');
-            return this.counter > 5 ? 'Greater than 5' : 'Less than or equal to 5';
-        }
-    }
+    el: '#app'
 });
