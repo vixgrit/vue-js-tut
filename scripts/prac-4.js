976f172477f2be8b49d5a78cb8630b26a89d29b2
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-05-31 01:16:36 -0400

    practice-4 styling solutions implemented

--- scripts/prac-4.js
+++ scripts/prac-4.js
@@ -1,11 +1,32 @@
 new Vue({
   el: '#exercise',
   data: {
-
+      pulseEffect: {
+          highlight: false,
+          shrink: true
+      },
+      turnBoxOrange: 'orangeBox',
+      divThree: '',
+      divFour: '',
+      addBorder: 'true',
+      changeHeight: '',
+      progressBar: 0
   },
   methods: {
     startEffect: function() {
-
+        var vi = this;
+        setInterval(function() {
+            vi.pulseEffect.highlight = !vi.pulseEffect.highlight;
+            vi.pulseEffect.shrink = !vi.pulseEffect.shrink
+        }, 1000);
+    },
+    startProgress: function() {
+        var vi = this;
+        setInterval(function() {
+            if (vi.progressBar < 100) {
+                vi.progressBar += 10;
+            }
+        }, 250);
     }
   }
 });
